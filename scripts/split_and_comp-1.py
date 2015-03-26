4758ee0c70198fe3aeea410309e67a293672d5b6
Zhang Hong <zong777@gmail.com>
2015-03-26 17:58:41 +0800

    sync

--- scripts/split_and_comp-1.py
+++ scripts/split_and_comp-1.py
@@ -17,7 +17,6 @@ def split_to_sentence(content):
     i = 0
     sentence_list = []
     punct_list = ',.!?:;~，。！？：；～'.decode('utf8')
-
     for word in content:
         if word in punct_list:
             sentence_list.append(content[start:i+1])
@@ -28,9 +27,68 @@ def split_to_sentence(content):
 
     if start < len(content):
         sentence_list.append(content[start:])
-
     return sentence_list
 
-text = "支持中國大陸.灣。香港！異體字和地區習慣用詞轉換"
-result = ";".join(split_to_sentence(text))
-print result
+def get_chemnoun(word_list, key_list):
+    chemnoun_list = []
+    for word in word_list:
+        word = word.strip()
+        for key in key_list:
+            key = key.strip()
+            if key in word:
+                chemnoun_list.append(word)
+                break
+    return chemnoun_list
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# Define which site we are dealing with By ourself!:
+site_name = raw_input('Please Input the site name: ')
+
+# Define where we put the files:
+source_dir = "/data/site_data/" + site_name
+keywords_file = "/data/paper/keywords/keywords.txt"
+user_dict = "/data/paper/keywords/user_dict.txt"
+result_dir = "/data/paper/result/" + site_name
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+result_file = result_dir + "/" + "result.txt"
+
+# Load userdict into jieba
+jieba.load_userdict(user_dict)
+
+# load the keywords file as a list
+f = file(keywords_file).decode('utf-8')
+key_list = f.readlines()
+f.close()
+
+# we put all the result in a delicated file for review:
+r = file(result_file, 'a')
+# We put one years's data into one file
+# And we make it compatible with Graphwiz's dot file
+for year in os.listdir(source_dir):
+    source_year = source_dir + '/' + year + '/'
+    result_year = result_dir + '/result-' + site_name + '-' + year + '.dot'
+    ry = open(result_year, "a")
+    file_head = "graph " + site_name + "-" + year + " {" + "\n"
+    file_root = "}"
+    ry.write(file_head)
+    # Get all the txt files in the dictionary:
+    for root, dirs, files in os.walk(source_year):
+        for file_name in files:
+            source_file = os.path.join(root, file_name)
+            source_article = open(source_file).read().decode('utf-8')
+            sentence_list = split_to_sentence(source_article)
+            # We need to know where are the keywords from:
+            r.write("The Title of the Content is %s : \n" % file_name)
+                for sentence in sentence_list:
+                    words_list = list(jieba.cut(sentence, cut_all=False))
+                    chemnoun_list = get_chemnoun(words_list, key_list)
+                    if result:
+                        result = ' -- '.join(result) + ';' + '\n'
+                        ry.writelines(result)
+                        r.writelines(result)
+    ry.write(file_root)
+    ry.close()
+r.close()
