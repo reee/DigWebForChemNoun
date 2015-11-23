1b996bab99ed3ad547bc9caa170420fe16d9fe3c
Zhang Hong <zong777@gmail.com>
2015-11-23 21:49:12 +0800

    prepare for the final shot!

--- scripts/split_and_comp.py
+++ scripts/split_and_comp.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
-# coding:utf-8
+#coding=utf-8
 
 import os
 import sys
 import jieba
+import jieba.posseg as posseg
 
-# Set the Default coding to utf-8
+from langconv import *
+from replace_dict import pattern_dict
+
+def replace_str(input_list, pattern_dict):
+    new_list = [pattern_dict[x] if x in pattern_dict else x for x in input_list]
+    return new_list
+
+def get_allnoun(content):
+    #Decode the text data to ascii
+    content = content.decode('utf8')
+    #Convert chinese traditional to chinese simplified
+    content = Converter('zh-hans').convert(content)
+    word_list = posseg.cut(content)
+    allnoun_list = []
+    for w in word_list:
+        if 'n' in w.flag:
+            allnoun_list.append(w.word)
+    return allnoun_list
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -19,34 +38,35 @@ user_dict = "/data/paper/keywords/user_dict.txt"
 result_dir = "/data/paper/result/" + site_name
 if not os.path.exists(result_dir):
     os.makedirs(result_dir)
-result_file = result_dir + "/" + "result.txt"
-
-# Load userdict into jieba
-jieba.load_userdict(user_dict)
 
 # Convert the Keywords file into a list
 keywords_list = open(keywords_file).readlines()
 keywords_list = [key.decode('utf-8').strip() for key in keywords_list]
 
+# Load userdict into jieba
+jieba.load_userdict(user_dict)
+
 # We put one years's data into one file
-# And it is compatible with Graphwiz's dot file
+# And we make it compatible with Graphwiz's dot file
 for year in os.listdir(source_dir):
-    source_year = source_dir + "/" + year + "/"
-    result_year = result_dir + "/result-" + site_name + "-" + year + ".dot"
-    r = open(result_year, "a")
-    file_head = "graph " + site_name + "-" + year + " {\n"
-    file_root = "}"
-    r.write(file_head)
+    source_year = source_dir + '/' + year + '/'
+    result_by_year = result_dir + '/result-' + site_name + '-' + year + '.dot'
+    f = open(result_by_year, "w")
+    file_head = "graph {\n"
+    file_end = "}"
+    chemnouns_list = []
     # Get all the txt files in the dictionary:
     for root, dirs, files in os.walk(source_year):
         for file_name in files:
             source_file = os.path.join(root, file_name)
-            source_article = open(source_file).read().decode('utf-8')
-            words_list = list(jieba.cut(source_article, cut_all=False))
-            result = list(set(words_list) & set(keywords_list))
-            # If the result is not empty, put into the file:
-            if result:
-                #r.write("# 本文标题为：%s \n " % file_name)
-                r.write(' -- '.join(result) + ";\n")
-    r.write(file_root)
-    r.close()
+            content = file(source_file).read()
+            allnoun_list = get_allnoun(content)
+            allnoun_list_rep = replace_str(allnoun_list, pattern_dict)
+            chemnoun_list = list(set(allnoun_list_rep) & set(keywords_list))
+            if chemnoun_list:
+                chemnouns = ' -- '.join(chemnoun_list) + ';\n'
+                chemnouns_list.append(chemnouns)
+    f.write(file_head)
+    f.writelines(chemnouns_list)
+    f.write(file_end)
+    f.close()
